9a2dfd852c2e351fd1afcf20ddb0fe060d233aad
Devonte <devontem@gmail.com>
2019-12-17 10:38:12 +0000

    Added initial useState and useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import cookie from './images/cookie.png';
 import './App.css';
 
 function App() {
+  // creating local state variables
+  let [allFortunes, setAllFortunes] = useState([]);
+  let [currentFortune, setCurrentFortune] = useState(null);
+
+  // fetching fortunes
+  useEffect(() => {
+    fetch('/fortunes.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setAllFortunes(data);
+      })
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={cookie} className="App-cookie" alt="cookie" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
