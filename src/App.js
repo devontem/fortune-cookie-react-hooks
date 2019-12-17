13c77cdee30766c96db41c83dd89c499ef1e17a5
Devonte <devontem@gmail.com>
2019-12-17 11:17:02 +0000

    Added logic for fortune cookie to be reset

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import cookie from './images/cookie.png';
+import cookieOpen from './images/cookie-open.png';
 import './App.css';
 
 function App() {
@@ -7,30 +8,53 @@ function App() {
   let [allFortunes, setAllFortunes] = useState([]);
   let [currentFortune, setCurrentFortune] = useState(null);
 
-  // fetching fortunes
+  // fetching all fortunes through a HTTP request
   useEffect(() => {
     fetch('/fortunes.json')
       .then((res) => res.json())
       .then((data) => {
         setAllFortunes(data);
-      })
+      });
   });
+
+  let fetchFortune = () => {
+    // fancy algorithm to get your unique fortune ;)
+    let randomIndex = Math.floor((Math.random() * allFortunes.length));
+    let fortune = allFortunes[randomIndex];
+    console.log(allFortunes, randomIndex, fortune)
+    setCurrentFortune(fortune);
+  }
+
+  let resetFortune = () => {
+    setCurrentFortune(null);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={cookie} className="App-cookie" alt="cookie" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {!currentFortune &&
+          <div onClick={fetchFortune} className="pointer">
+            <img src={cookie} className="App-logo" alt="cookie" />
+          </div>
+        }
+
+        {currentFortune &&
+          <div>
+            <div>
+              <img src={cookieOpen} className="App-logo no-animation fade-in" alt="cookie" />
+            </div>
+            <p className="fade-in">
+              {currentFortune}
+            </p>
+            <a
+              className="App-link fade-in-slow pointer"
+              onClick={resetFortune}
+            >
+              Don't like it? Try again...
+            </a>
+          </div>
+        }
       </header>
     </div>
   );
